940071076bca85f6e6d8c8da63a057053dde9c7b
Najszybciej! Najbardziej! dostępne rynkowo usługi finansowe na zakupy wedle Państwa finansowych i negocjacyjno - zakupowych oczekiwań   :) <profesja.premium@gmail.com>
2026-03-11 20:20:37 +0100

    Create script.js

---
+++ script.js
@@ -0,0 +1,14 @@
+function raty(){
+
+alert("Kalkulator rat będzie dostępny wkrótce")
+
+}
+
+
+document.getElementById("form").addEventListener("submit",function(e){
+
+e.preventDefault()
+
+alert("Wiadomość wysłana")
+
+})
